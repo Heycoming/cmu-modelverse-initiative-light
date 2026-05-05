de43808892c933d68a64eb8c45500dc50174cd50
Tianqin Li <jacklitianqin@gmail.com>
2026-05-05 04:41:03 -0400

    update

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -15,7 +15,7 @@ const Team = () => {
     {
       name: "Tianqin Li",
       role: "Chief Scientist",
-      bio: "PhD Researcher in CMU CS Department under Tai Sing Lee (NeurIPS Neuroscience ex-Director) and Zico Kolter (CMU AI Area Director in Computer Science, OpenAI Borad). Collaborated with Ruslan Salakhutdinov (VP Research @ Meta, ex-Apple AI Director, Student of AI's Founding Father, Geoffrey Hinton) and Louis-Philippe Morency (Multimodal AI pioneer professor). Tianqin is also a fellow at Y Combinator. Research focuses on AI models and human intelligence. NeurIPS 2023 Oral presentation (selective 1% of all submissions). Guest lecturer in multiple CMU AI courses.",
+      bio: "PhD Researcher in CMU CS Department. Work with Tai Sing Lee (NeurIPS Neuroscience ex-Director), Zico Kolter (CMU AI Area Director in Computer Science, OpenAI Borad), Ruslan Salakhutdinov (VP Research @ Meta, ex-Apple AI Director, Student of AI's Founding Father, Geoffrey Hinton) and Louis-Philippe Morency (Multimodal AI pioneer professor). Tianqin is also a fellow at Y Combinator. Research focuses on AI models and human intelligence. NeurIPS 2023 Oral presentation (selective 1% of all submissions). Guest lecturer in multiple CMU AI courses.",
       links: { linkedin: "https://www.linkedin.com/in/tianqin-li-b16299170/", website: "http://crazy-jack.github.io/works.html" }
     },
     {
